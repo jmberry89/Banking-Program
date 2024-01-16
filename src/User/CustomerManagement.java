cf1c5d7b31ab37f5c6cacb7386ab36aa3c8a85db
jmberry89 <jmberry89@protonmail.com>
2024-01-16 20:38:16 +0100

    updated CustomerManagement

--- src/User/CustomerManagement.java
+++ src/User/CustomerManagement.java
@@ -1,25 +1,64 @@
 package User;
+import java.util.ArrayList;
 
 import java.util.List;
 
 public class CustomerManagement implements UserManagement{
-    private List<User> userdatabase;
+    private List<User> userdatabase = new ArrayList<User>();
     @Override
     public void addUser(User user) {
         this.userdatabase.add(user);
     }
 
     @Override
-    public void removeUser() {
+    public void removeUser(String username) {
+        if(this.verifyUser(username)){
+            int index = this.getIndex(username);
+            this.userdatabase.remove(index);
+        }
+        else{
+            System.out.println("User does not exist");
+        }
 
     }
 
     @Override
-    public int findUser(String username) {
-        int index = 0;
-        while(this.userdatabase.get(index).getUsername()!= username){
-
+    public int getIndex(String username) {
+        if(this.verifyUser(username)){
+            for(int index = 0; index < this.userdatabase.size(); index++) {
+                if (this.userdatabase.get(index).getUsername() == username) {
+                    return index;
+                }
+            }
         }
-        return index;
+        System.out.println("Error: User does not exist");
+        return -1;
+    }
+
+    @Override
+    public boolean verifyUser(String username) {
+        if(this.userdatabase.get(0) == null){
+            return false;
+        }
+        for(int index = 0; index < this.userdatabase.size(); index++){
+            if(this.userdatabase.get(index).getUsername() == username){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+    @Override
+    public User getUser(String username) {
+        if(this.verifyUser(username)){
+            int index = this.getIndex(username);
+            return this.userdatabase.get(index);
+        }
+        else{
+            System.out.println("Error: User " + username + " does not exist.");
+            return null;
+        }
     }
 }
--- src/User/UserManagement.java
+++ src/User/UserManagement.java
@@ -2,6 +2,8 @@ package User;
 
 public interface UserManagement {
     void addUser(User user);
-    void removeUser();
-    int findUser(String username);
+    void removeUser(String username);
+    int getIndex(String username);
+    boolean verifyUser(String username);
+    User getUser(String username);
 }
